0d8163b8a994a4ed017c0455dd50a8a24c92c4bc
Rebecca Cohen <rebecca.cohen@duke.edu>
2018-10-24 10:39:13 -0400

    Began debugging unit tests for data validation

--- test_hrm_reader.py
+++ test_hrm_reader.py
@@ -1,13 +1,16 @@
 from hrm_reader import import_data
 import numpy
+import pytest
 
 def test_data_simple():
     data = import_data("test_hrm_reader_data3.csv")
     numpy.testing.assert_array_equal(data, [(1, 2), (3, 4), (5, 6)])
 
 def test_data_floatsOnly():
-    data1 = import_data("test_hrm_reader_data1.csv")
+    with pytest.raises(ValueError):
+        data1 = import_data("test_hrm_reader_data1.csv")
 
 def test_data_extraColumn():
-    data2 = import_data("test_hrm_reader_data2.csv")
+    with pytest.raises(ValueError):
+        data2 = import_data("test_hrm_reader_data2.csv")
 
