f7d9f5e70166f627d80a0f5e806e3f4be70d6ff9
Mark Palmeri <mlp6@duke.edu>
2018-10-24 15:25:01 -0400

    clean up try/except clause
    * rm else for return statement
    * push through raised ValueError
    * added docstring w/ Raises info

--- hrm_reader.py
+++ hrm_reader.py
@@ -1,9 +1,20 @@
 def import_data(file):
+    """load in CSV data
+
+    Args:
+        file (str): filename
+
+    Raises:
+        ValueError on ill-conditioned CSV file
+
+    Returns:
+        data (ndarray)
+    """
     import numpy as np
 
     try:
         data = np.loadtxt(file, delimiter=',')
     except ValueError:
-       print("CSV file must only contain numbers")
-    else:
-        return data
+        raise ValueError("CSV file must only contain numbers")
+
+    return data
