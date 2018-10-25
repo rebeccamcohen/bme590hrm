ff354ad75a4678dc2a12a13a8b3bdddb0fa3100c
Rebecca Cohen <rebecca.cohen@duke.edu>
2018-10-25 14:25:09 -0400

    Completed unit test for writer

--- hrm_writer.py
+++ hrm_writer.py
@@ -2,4 +2,4 @@ import json
 
 def write_jsonfile(dict,filename):
     with open((filename + '.json'), 'w') as outfile:
-    json.dump(dict, outfile, sort_keys=True, indent=4)
+        json.dump(dict, outfile, sort_keys=True, indent=4)
--- test_hrm_writer.py
+++ test_hrm_writer.py
@@ -1,5 +1,23 @@
 from hrm_writer import write_jsonfile
+import json
 
 def test_write_jsonfile():
-    dict = {}
+    test_dict = {}
+    test_dict["Name"] = "Rebecca"
+    test_dict["Last Name"] = "Cohen"
+    test_dict["Age"] = 21
+    test_dict["Tuple"] = [1,2]
+    test_dict["Empty"] = []
+    test_dict["Nested"] = {}
+    test_dict["Nested"]["a"] = 1
+    test_dict["Nested"]["b"] = "It is cool that dictionaries can be nested"
+    func_result = write_jsonfile(test_dict, "test_jsonfile")
+
+    with open('test_jsonfile.json', 'r') as readfile:
+        read_test_dict = json.load(readfile)
+
+    assert test_dict == read_test_dict
+
+
+
 
