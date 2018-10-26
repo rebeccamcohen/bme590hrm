134648b7011ee6f942157c602d4942e7a270673a
Rebecca Cohen <rebecca.cohen@duke.edu>
2018-10-25 20:41:27 -0400

    Completing processing with window strategy

--- hrm_processor.py
+++ hrm_processor.py
@@ -1,6 +1,6 @@
-#in numpy array: axis=0 refers to rows and axis=1 refers to columns
 import numpy
 import peakutils
+
 
 def find_min_max(data):
     minVoltage = numpy.amin(data[:, 1])
@@ -8,14 +8,16 @@ def find_min_max(data):
     extremes = (minVoltage, maxVoltage)
     return extremes
 
+
 def find_duration(data):
-    len_time = len(data[:,1])
-    duration = data[len_time-1,0] - data[0,0]
+    len_time = len(data[:, 1])
+    duration = data[len_time-1, 0] - data[0, 0]
     return duration
 
+
 def find_windowlen(data):
-    numTime = len(data[:,0])
-    numVoltage = len(data[:,1])
+    numTime = len(data[:, 0])
+    numVoltage = len(data[:, 1])
     if numVoltage != numTime:
         numEntries = min(numData, numTime)
     else:
@@ -24,8 +26,9 @@ def find_windowlen(data):
     windowlen = round(numEntries/5)
     return windowlen
 
+
 def divide_data(data, windowlen):
-    w1 = data[0:windowlen,:]
+    w1 = data[0:windowlen, :]
     w2 = data[windowlen:windowlen*2, :]
     w3 = data[(windowlen * 2):windowlen * 3, :]
     w4 = data[(windowlen * 3):windowlen * 4, :]
@@ -33,9 +36,10 @@ def divide_data(data, windowlen):
 
     return w1, w2, w3, w4, w5
 
+
 def find_peak_indices(w1, w2, w3, w4, w5):
 
-    ind_w1 = peakutils.indexes(w1[:,1], thres=0.5 * numpy.amax(w1[:,1]))
+    ind_w1 = peakutils.indexes(w1[:, 1], thres=0.5 * numpy.amax(w1[:, 1]))
     ind_w2 = peakutils.indexes(w2[:, 1], thres=0.5 * numpy.amax(w2[:, 1]))
     ind_w3 = peakutils.indexes(w3[:, 1], thres=0.5 * numpy.amax(w3[:, 1]))
     ind_w4 = peakutils.indexes(w4[:, 1], thres=0.5 * numpy.amax(w4[:, 1]))
@@ -43,31 +47,38 @@ def find_peak_indices(w1, w2, w3, w4, w5):
 
     return ind_w1, ind_w2, ind_w3, ind_w4, ind_w5
 
-def find_num_peaks(ind_w1, ind_w2, ind_w3, ind_w4, ind_w5):
-    total_numPeaks = len(ind_w1) + len(ind_w2) + len(ind_w3) + len(ind_w4) + len(ind_w5)
-    return total_numPeaks, len(ind_w1), len(ind_w2), len(ind_w3), len(ind_w4), len(ind_w5)
 
-    #if len(i) == 0:
-        #raise TypeError
-    #else:
-        #return len(i)
+def find_num_peaks_window(ind_w1, ind_w2, ind_w3, ind_w4, ind_w5):
+    return len(ind_w1), len(ind_w2), len(ind_w3), len(ind_w4), len(ind_w5)
 
-def find_time_of_peaks(w1, w2, w3, w4, w5, ind_w1, ind_w2, ind_w3, ind_w4, ind_w5):
-    time_of_peaks1 = w1[ind_w1,0]
+
+def find_num_peaks_total(ind_w1, ind_w2, ind_w3, ind_w4, ind_w5):
+    return len(ind_w1) + len(ind_w2) + len(ind_w3) + len(ind_w4) + len(ind_w5)
+
+
+def find_time_of_peaks(w1, w2, w3, w4, w5,
+                       ind_w1, ind_w2, ind_w3, ind_w4, ind_w5):
+    time_of_peaks1 = w1[ind_w1, 0]
     time_of_peaks2 = w2[ind_w2, 0]
     time_of_peaks3 = w3[ind_w3, 0]
     time_of_peaks4 = w4[ind_w4, 0]
     time_of_peaks5 = w5[ind_w5, 0]
 
-    time_of_peaks = numpy.concatenate((time_of_peaks1, time_of_peaks2, time_of_peaks3, time_of_peaks4, time_of_peaks5))
+    time_of_peaks = numpy.concatenate((time_of_peaks1, time_of_peaks2,
+                                       time_of_peaks3, time_of_peaks4,
+                                       time_of_peaks5))
     return time_of_peaks
 
+
 def find_bpm(duration, total_numPeaks):
-    duration_inMins = duration/60
-    bpm = total_numPeaks/duration_inMins
+
+    duration_inmins = duration / 60
+    bpm = total_numPeaks / duration_inmins
+
     return bpm
 
-def create_metrics_dictionary(a,b,c,d,e):
+
+def create_metrics_dictionary(a, b, c, d, e):
     metrics_dict = {}
     metrics_dict["mean_hr_bpm"] = a
     metrics_dict["voltage_extremes"] = b
