fe4f55922ca47bae03763c66309c97f857fa645d
Rebecca Cohen <rebecca.cohen@duke.edu>
2018-10-26 00:47:36 -0400

    Add documentation

--- hrm_writer.py
+++ hrm_writer.py
@@ -1,5 +1,16 @@
 import json
 
-def write_jsonfile(dict,filename):
+
+def write_jsonfile(dict, filename):
+    """
+    Args:
+        dict (dict): dictionary consisting of calculated metrics
+        filename (str): name of input CSV file
+
+    Returns:
+        file (json): contains values of object attributes
+
+    """
+
     with open((filename + '.json'), 'w') as outfile:
         json.dump(dict, outfile, sort_keys=True, indent=4)
--- test_hrm_writer.py
+++ test_hrm_writer.py
@@ -1,12 +1,13 @@
 from hrm_writer import write_jsonfile
 import json
 
+
 def test_write_jsonfile():
     test_dict = {}
     test_dict["Name"] = "Rebecca"
     test_dict["Last Name"] = "Cohen"
     test_dict["Age"] = 21
-    test_dict["Tuple"] = [1,2]
+    test_dict["Tuple"] = [1, 2]
     test_dict["Empty"] = []
     test_dict["Nested"] = {}
     test_dict["Nested"]["a"] = 1
@@ -17,7 +18,3 @@ def test_write_jsonfile():
         read_test_dict = json.load(readfile)
 
     assert test_dict == read_test_dict
-
-
-
-
